02c3786a1fa0088a71795d743513157d145a650e
ju8ko <julia.x.kobzar@gmail.com>
2022-07-06 17:02:11 +0200

    Tasks 1-2 are done

---
+++ solution.js
@@ -0,0 +1,9 @@
+//1
+let stringNumber = "3";
+console.log("Ex.1.1:", stringNumber == 3);
+console.log("Ex.1.2.:", stringNumber === 3);
+console.log("Ex.1.3.:", 2, 7, 9);
+//2
+let myVar = true;
+console.log("Ex.2:", myVar === !true ? "good morning" : "good evening");
+//3
